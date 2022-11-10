8f10a170e706b5dac7a54902dcbb272f39694b37
ndionis <ndionis@k1r3p7.42mulhouse.fr>
2022-11-10 11:15:55 +0100

    algo looks better (even in theory)

--- src/raycasting.c
+++ src/raycasting.c
@@ -16,12 +16,14 @@ void	set_delta_distance(t_data *data)
 {
 	t_ray 	*ray;
 	t_cam	*cam;
+	double	u;
 	
 	cam = data->cam;
 	ray = cam->beam;
+	u = (double)UNITS_PER_BOX;
 	ray->direction_len = vec_len(ray->direction);
-	ray->delta_distances.x = ray->direction_len / ray->direction.x;
-	ray->delta_distances.y = ray->direction_len / ray->direction.y;
+	ray->delta_distances.x = ray->direction_len * u / ray->direction.x;
+	ray->delta_distances.y = ray->direction_len * u / ray->direction.y;
 	ray->vector_deltaX = vec_scale(ray->direction, ray->delta_distances.x / ray->direction_len);
 	ray->vector_deltaY = vec_scale(ray->direction, ray->delta_distances.y / ray->direction_len);
 }
@@ -35,12 +37,20 @@ void	set_side_distance(t_data *data)
 	t_cam		*cam;
 	t_ray		*ray;
 	t_player	*player;
+	double		u;
 	
 	cam = data->cam;
 	ray = cam->beam;
 	player = data->player;
-	ray->side_distances.x = ray->delta_distances.x * (double)(UNITS_PER_BOX - player->pos_box.x) / (double)UNITS_PER_BOX;
-	ray->side_distances.y = ray->delta_distances.y * (double)(UNITS_PER_BOX - player->pos_box.y) / (double)UNITS_PER_BOX;
+	u = (double)UNITS_PER_BOX;
+	if (ray->direction.x > 0)
+		ray->side_distances.x = ray->delta_distances.x * (double)(u - player->pos_box.x) / u;
+	else
+		ray->side_distances.x = ray->delta_distances.x * (double)(1 + player->pos_box.x) / u;
+	if (ray->direction.y > 0)
+		ray->side_distances.y = ray->delta_distances.y * (double)(u - player->pos_box.y) / u;
+	else
+		ray->side_distances.y = ray->delta_distances.y * (double)(1 + player->pos_box.y) / u;
 	ray->vector_sideX = vec_scale(ray->direction, ray->side_distances.x / ray->direction_len);
 	ray->vector_sideY = vec_scale(ray->direction, ray->side_distances.y / ray->direction_len);
 }
