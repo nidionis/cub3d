2e6c7fc58092d476c895af950f49838320ed6aec
ndionis <ndionis@k1r3p7.42mulhouse.fr>
2022-11-12 18:27:51 +0100

    malloced structures obstacle are freed in clean exit

--- src/exit_ft.c
+++ src/exit_ft.c
@@ -30,10 +30,15 @@ void	clean_exit(t_data *data, int exit_code)
 			free(data->blocks);
 		if (data->map_cases)
 			free(data->map_cases);
-		if (data->cam->beam)
-			free(data->cam->beam);
-		if (data->cam->beam)
+		if (data->cam)
+		{
+			if (data->cam->beam)
+				free(data->cam->beam);
+			i = 0;
+			while (i < CAM_QUALITY)
+				ft_lstclear(&data->cam->arRay[i++].obstacles_ls);
 			free(data->cam);
+		}
 		if (data->image)
 		{
 			i = 0;
