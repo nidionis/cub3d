32cfa367ba75d9d90e0d2e97f2ad6e6e4bb0f6e5
presk0 <contact@presko.info>
2022-12-07 12:50:13 +0100

    before checkout

--- src/bonus_textures.c
+++ src/bonus_textures.c
@@ -22,3 +22,26 @@ void	import_bonus_textures(t_data *data)
 	}
 	ft_free_split(&fnames);
 }
+
+void	import_sprites_textures(t_data *data)
+{
+	int			i;
+	char		**fnames;
+	int		nb_textures;
+	t_img_data	t;
+
+	i = 0;
+	fnames = ft_split(SPRITES_TEXTURES, ',');
+	nb_textures = ft_matrixlen(fnames);
+	data->bonus_textures = malloc(sizeof(t_img_data) * nb_textures);
+	while (i < nb_textures)
+	{
+		t.img = mlx_xpm_file_to_image(data->window->mlx, fnames[i], &t.line_len, &t.line_height);
+		t.adress = mlx_get_data_addr(t.img, &t.bpp, &t.line_len, &t.endian);
+		if (!t.img)
+			exit_msg(data, "[import_textures] Pb loading xpm file", 1);
+		data->bonus_textures[i] = t;
+		i++;
+	}
+	ft_free_split(&fnames);
+}
