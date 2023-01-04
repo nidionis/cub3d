c392cd51389285832e4d0f169d58a3d3d4870643
ndionis <ndionis@k1r3p7.42mulhouse.fr>
2023-01-04 16:11:50 +0100

    clean exit is only used in malloc_data

--- src_bonus/exit_ft.c
+++ src_bonus/exit_ft.c
@@ -6,7 +6,7 @@
 /*   By: dpaulino <dpaulino@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 15:17:56 by supersko          #+#    #+#             */
-/*   Updated: 2023/01/02 12:54:43 by dpaulino         ###   ########.fr       */
+/*   Updated: 2023/01/04 16:03:10 by supersko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,14 @@
 /* where all pointers are cleaned, especially in a emergency exit */
 void	clean_exit(t_data *data, int exit_code)
 {
-	int	i;
-
 	if (data)
 	{
-		if (data->line)
-			free(data->line);
-		if (data->line_split)
-			ft_free_split(&data->line_split);
-		if (data->map)
-			ft_free_split(&data->map);
+		if (data->image)
+			free(data->image);
 		if (data->player)
 			free(data->player);
+		if (data->map)
+			ft_free_split(&data->map);
 		if (data->blocks)
 			free(data->blocks);
 		if (data->map_cases)
@@ -35,21 +31,7 @@ void	clean_exit(t_data *data, int exit_code)
 		{
 			if (data->cam->beam)
 				free(data->cam->beam);
-			i = 0;
-			while (i < CAM_QUALITY)
-				ft_lstclear(&data->cam->array[i++].obstacles_ls, free);
 			free(data->cam);
-		}
-		if (data->image)
-		{
-			i = 0;
-			while (i < NB_TEXTURES)
-			{
-				if (data->image->texture_path[i])
-					free(data->image->texture_path[i]);
-				i++;
-			}
-			free(data->image);
 		}
 		free(data);
 	}
